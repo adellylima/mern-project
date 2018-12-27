925d2cb3b19aea859745f714a36560a6340ea106
adellylima <adellylima@hotmail.com>
2018-12-27 16:08:56 -0300

    Prepared for deploy

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
-module.exports = {
-    mongoURI: 'mongodb://127.0.0.1:27017/devprogram',
-    secretOrKey: 'secret'
- 
-};
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod');
+  } else {
+    module.exports = require('./keys_dev');
+  }
+  
--- server.js
+++ server.js
@@ -28,10 +28,21 @@ app.use(passport.initialize());
 // Passport Config
 require('./config/passport')(passport);
 
+
 // Use Routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
